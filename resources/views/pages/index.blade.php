1acbdc982636ed0d7864a05d3ade4e9e9aab3db1
manirabona-programer <hseal419@gmail.com>
2023-05-29 10:27:19 +0200

    chore(analytics): add google tag manager

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -53,6 +53,14 @@
 
         gtag('config', 'G-BHRK59S1F7');
     </script>
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-N96KD8Q');</script>
+    <!-- End Google Tag Manager -->
 @stop
 
 @section('title')
@@ -64,7 +72,7 @@
     <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N96KD8Q"
         height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
-    
+
     <x-navigation-bar />
 
     <main>
